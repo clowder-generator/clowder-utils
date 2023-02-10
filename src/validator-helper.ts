25169705d1b8914b2330c4e5c315e869644035f9
Leddzip <leddzip@gmail.com>
2023-02-10 23:02:01 +0100

    feature/CLOWDER-18_common-method-to-validate-input-for-prompt
    doc: adding some doc string for each public methods of the validator helper

--- src/validator-helper.ts
+++ src/validator-helper.ts
@@ -7,11 +7,39 @@ import {
     snakeCaseRegex
 } from './case-helper';
 
+/**
+ * Type representing a validation function for yeoman or inquirer.js.
+ * A validation function is a function that take a string as argument
+ * and return either a Promise<true> if the function application on
+ * the input is valid, or a Promise<string> if not. If not valid,
+ * the string should contain an explanation on why it is not valid.
+ */
 export type stringValidationFunction = (input: string) => Promise<true | string>;
 
+/**
+ * Options to pass to `validateWith` in order to transform the input before applying the
+ * a validation function, or to alter the error message to display in case of invalid input
+ */
 export interface ValidationOption {
+    /**
+     * Defines if the input should be trimmed of white spaces
+     * before applying a validation function. If not defined,
+     * the input will not be trimmed.
+     */
     trimmed?: boolean;
+
+    /**
+     * Define what will be the error message to display in case of
+     * invalid input. If not defined, there will not be a global error.
+     */
     globalErrorMessage?: string;
+
+    /**
+     * Define if the displayed error message should be displayed
+     * when a global error message is defined. If not set,
+     * or if global error message is not set, then the
+     * original error message will be displayed.
+     */
     hideRootCause?: boolean;
 }
 
@@ -29,6 +57,19 @@ const globalAndDefaultErrorMessage = (validationOption: ValidationOption | undef
     );
 };
 
+/**
+ * validateWith
+ *
+ * Higher order function that will apply all the given validation function on the input
+ * and will format the error message, if any, according to the validationOption.
+ *
+ * @param funcs: a list of validation function to apply on the input
+ * @param opt: an optional parameter to format the input before applying the functions or
+ *             to format the error if any
+ *
+ * @return a new stringValidationFunction that will the the composition of all the validation functions
+ *         given into parameter
+ */
 export const validateWith = (funcs: stringValidationFunction[], opt?: ValidationOption): stringValidationFunction => {
     return async (input: string): Promise<true | string> => {
         const transformedInput = opt?.trimmed ? input.trim() : input;
@@ -51,6 +92,17 @@ export const validateWith = (funcs: stringValidationFunction[], opt?: Validation
     };
 };
 
+/**
+ * shouldMatchRegexValidation
+ *
+ *  Higher order function that return a stringValidationFunction validating if the input match the given regex
+ *
+ * @param regex: the regex to match fo the stringValidationFunction to return true
+ * @param errorMessageFormat: optional. The error to display if it does not match. Use %s in the error string
+ *                            to display the input in the error message.
+ *
+ * @return a stringValidationFunction that test if the input match the regex.
+ */
 export const shouldMatchRegexValidation = (regex: RegExp, errorMessageFormat?: string): stringValidationFunction => {
     return async (input: string): Promise<true | string> => {
         if (!regex.test(input)) {
@@ -63,6 +115,18 @@ export const shouldMatchRegexValidation = (regex: RegExp, errorMessageFormat?: s
     };
 };
 
+/**
+ * shouldNotMatchRegexValidation
+ *
+ * Higher order function that take a regex in parameter and return a stringValidationFunction that
+ * test that its input does not match the regex.
+ *
+ * @param regex: the regex that should not be matched by the input
+ * @param errorMessageFormat: optional. An error message to display if the input match the regex. Use %s in the error
+ *                            string to display the input in the error message.
+ *
+ * @return a stringValidationFunction that test if the input does not match the regex
+ */
 export const shouldNotMatchRegexValidation = (regex: RegExp, errorMessageFormat?: string): stringValidationFunction => {
     return async (input: string): Promise<true | string> => {
         if (regex.test(input)) {
@@ -75,6 +139,17 @@ export const shouldNotMatchRegexValidation = (regex: RegExp, errorMessageFormat?
     };
 };
 
+/**
+ * nonBlankValidation
+ *
+ * Higher order function. The resulting function verify that its input is not a blank string. If it is, it
+ * will return the given error message
+ *
+ * @param errorMessage: optional. An error message to display if the input is blank. Use %s in the error string
+ *                      to display the input in the error message.
+ *
+ * @return a stringValidationFunction that test that its input is not blank.
+ */
 export const nonBlankValidation = (errorMessage?: string): stringValidationFunction => {
     return async (input: string): Promise<true | string> => {
         if (/^\s*$/.test(input)) {
@@ -85,6 +160,17 @@ export const nonBlankValidation = (errorMessage?: string): stringValidationFunct
     };
 };
 
+/**
+ * noWhiteSpaceValidation
+ *
+ * Higher order function that return a stringValidationFunction checking that its input
+ * does not contain white spaces.
+ *
+ * @param errorMessage: optional. An error to display if the input contains white chars. Use %s in the error string
+ *                      to display the input in the error message.
+ *
+ * @return a stringValidationFunction that verify if the input does not contain a white char.
+ */
 export const noWhiteSpaceValidation = (errorMessage?: string): stringValidationFunction => {
     return async (input: string): Promise<true | string> => {
         if (/^.*\s+.*$/.test(input)) {
@@ -97,6 +183,17 @@ export const noWhiteSpaceValidation = (errorMessage?: string): stringValidationF
     };
 };
 
+/**
+ * doNotStartWithNumberValidation
+ *
+ * Higher order function that return a stringValidationFunction checking that its input
+ * does not start with a number.
+ *
+ * @param errorMessage: optional. An error to display if the input starts with a number. Use %s in the error string
+ *                      to display the input in the error message.
+ *
+ * @return a stringValidationFunction that verify if the input does not start with a number.
+ */
 export const doNotStartWithNumberValidation = (errorMessage?: string): stringValidationFunction => {
     return async (input: string): Promise<true | string> => {
         if (/^\d.*$/.test(input)) {
@@ -109,6 +206,17 @@ export const doNotStartWithNumberValidation = (errorMessage?: string): stringVal
     };
 };
 
+/**
+ * noTrailingWhiteSpaceValidation
+ *
+ * Higher order function that return a stringValidationFunction checking that its input
+ * does not end with white spaces.
+ *
+ * @param errorMessage: optional. An error to display if the input end with white chars. Use %s in the error string
+ *                      to display the input in the error message.
+ *
+ * @return a stringValidationFunction that verify if the input does not end with a white char.
+ */
 export const noTrailingWhiteSpaceValidation = (errorMessage?: string): stringValidationFunction => {
     return async (input: string): Promise<true | string> => {
         if (/^.*\s$/.test(input)) {
@@ -121,6 +229,17 @@ export const noTrailingWhiteSpaceValidation = (errorMessage?: string): stringVal
     };
 };
 
+/**
+ * noLeadingWhiteSpaceValidation
+ *
+ * Higher order function that return a stringValidationFunction checking that its input
+ * does not start with white spaces.
+ *
+ * @param errorMessage: optional. An error to display if the input start with white chars. Use %s in the error string
+ *                      to display the input in the error message.
+ *
+ * @return a stringValidationFunction that verify if the input does not start with a white char.
+ */
 export const noLeadingWhiteSpaceValidation = (errorMessage?: string): stringValidationFunction => {
     return async (input: string): Promise<true | string> => {
         if (/^\s.*$/.test(input)) {
@@ -132,6 +251,19 @@ export const noLeadingWhiteSpaceValidation = (errorMessage?: string): stringVali
     };
 };
 
+/**
+ * noInnerWhiteSpaceValidation
+ *
+ * Higher order function that return a stringValidationFunction checking that its input
+ * does not contain white spaces in the middle of the string only (leading and trailing are
+ * still allowed).
+ *
+ * @param errorMessage: optional. An error to display if the input contains white chars in the middle of the string.
+ *                      Use %s in the error string to display the input in the error message.
+ *
+ * @return a stringValidationFunction that verify if the input does not contain a white char in the middle of the
+ *         string.
+ */
 export const noInnerWhiteSpaceValidation = (errorMessage?: string): stringValidationFunction => {
     return async (input: string): Promise<true | string> => {
         if (/.*\S\s+\S.*/.test(input)) {
@@ -144,6 +276,17 @@ export const noInnerWhiteSpaceValidation = (errorMessage?: string): stringValida
     };
 };
 
+/**
+ * kebabCaseValidation
+ *
+ * Higher order function that return a stringValidationFunction checking that its input
+ * matches a kebab case.
+ *
+ * @param errorMessage: optional. An error to display if the input does not match a kebab case.
+ *                      Use %s in the error string to display the input in the error message.
+ *
+ * @return a stringValidationFunction that verify if the input matches a kebab case.
+ */
 export const kebabCaseValidation = (errorMessage?: string): stringValidationFunction => {
     return async (input: string): Promise<true | string> => {
         if (!kebabCaseRegex.test(input)) {
@@ -156,6 +299,17 @@ export const kebabCaseValidation = (errorMessage?: string): stringValidationFunc
     };
 };
 
+/**
+ * screamingKebabCaseValidation
+ *
+ * Higher order function that return a stringValidationFunction checking that its input
+ * matches a screaming kebab case.
+ *
+ * @param errorMessage: optional. An error to display if the input does not match a screaming kebab case.
+ *                      Use %s in the error string to display the input in the error message.
+ *
+ * @return a stringValidationFunction that verify if the input matches a screaming kebab case.
+ */
 export const screamingKebabCaseValidation = (errorMessage?: string): stringValidationFunction => {
     return async (input: string): Promise<true | string> => {
         if (!screamingKebabCaseRegex.test(input)) {
@@ -168,6 +322,17 @@ export const screamingKebabCaseValidation = (errorMessage?: string): stringValid
     };
 };
 
+/**
+ * snakeCaseValidation
+ *
+ * Higher order function that return a stringValidationFunction checking that its input
+ * matches a snake case.
+ *
+ * @param errorMessage: optional. An error to display if the input does not match a snake case.
+ *                      Use %s in the error string to display the input in the error message.
+ *
+ * @return a stringValidationFunction that verify if the input matches a snake case.
+ */
 export const snakeCaseValidation = (errorMessage?: string): stringValidationFunction => {
     return async (input: string): Promise<true | string> => {
         if (!snakeCaseRegex.test(input)) {
@@ -180,6 +345,17 @@ export const snakeCaseValidation = (errorMessage?: string): stringValidationFunc
     };
 };
 
+/**
+ * screamingSnakeCaseValidation
+ *
+ * Higher order function that return a stringValidationFunction checking that its input
+ * matches a screaming snake case.
+ *
+ * @param errorMessage: optional. An error to display if the input does not match a screaming snake case.
+ *                      Use %s in the error string to display the input in the error message.
+ *
+ * @return a stringValidationFunction that verify if the input matches a screaming snake case.
+ */
 export const screamingSnakeCaseValidation = (errorMessage?: string): stringValidationFunction => {
     return async (input: string): Promise<true | string> => {
         if (!screamingSnakeCaseRegex.test(input)) {
@@ -192,6 +368,17 @@ export const screamingSnakeCaseValidation = (errorMessage?: string): stringValid
     };
 };
 
+/**
+ * camelCaseValidation
+ *
+ * Higher order function that return a stringValidationFunction checking that its input
+ * matches a camel case.
+ *
+ * @param errorMessage: optional. An error to display if the input does not match a camel case.
+ *                      Use %s in the error string to display the input in the error message.
+ *
+ * @return a stringValidationFunction that verify if the input matches a camel case.
+ */
 export const camelCaseValidation = (errorMessage?: string): stringValidationFunction => {
     return async (input: string): Promise<true | string> => {
         if (!camelCaseRegex.test(input)) {
@@ -204,6 +391,17 @@ export const camelCaseValidation = (errorMessage?: string): stringValidationFunc
     };
 };
 
+/**
+ * pascalCaseValidation
+ *
+ * Higher order function that return a stringValidationFunction checking that its input
+ * matches a pascal case.
+ *
+ * @param errorMessage: optional. An error to display if the input does not match a pascal case.
+ *                      Use %s in the error string to display the input in the error message.
+ *
+ * @return a stringValidationFunction that verify if the input matches a pascal case.
+ */
 export const pascalCaseValidation = (errorMessage?: string): stringValidationFunction => {
     return async (input: string): Promise<true | string> => {
         if (!pascalCaseRegex.test(input)) {
@@ -216,6 +414,18 @@ export const pascalCaseValidation = (errorMessage?: string): stringValidationFun
     };
 };
 
+/**
+ * integerValidation
+ *
+ * Higher order function that return a stringValidationFunction checking that its input
+ * is an integer. An integer being define as a number, either positive or negative, with no
+ * decimal part.
+ *
+ * @param errorMessage: optional. An error to display if the input is not an integer.
+ *                      Use %s in the error string to display the input in the error message.
+ *
+ * @return a stringValidationFunction that verify if the input is an integer.
+ */
 export const integerValidation = (errorMessage?: string): stringValidationFunction => {
     return async (input: string): Promise<true | string> => {
         if (!isFiniteNumber(input)) {
@@ -233,6 +443,18 @@ export const integerValidation = (errorMessage?: string): stringValidationFuncti
     };
 };
 
+/**
+ * naturalNumberValidation
+ *
+ * Higher order function that return a stringValidationFunction checking that its input
+ * is a natural number. A natural number being define as a strictly positive number (above zero, zero excluded),
+ * with no decimal part.
+ *
+ * @param errorMessage: optional. An error to display if the input is not a natural number.
+ *                      Use %s in the error string to display the input in the error message.
+ *
+ * @return a stringValidationFunction that verify if the input is a natural number.
+ */
 export const naturalNumberValidation = (errorMessage?: string): stringValidationFunction => {
     return async (input: string): Promise<true | string> => {
         if (!isFiniteNumber(input)) {
@@ -251,6 +473,17 @@ export const naturalNumberValidation = (errorMessage?: string): stringValidation
     };
 };
 
+/**
+ * numberValidation
+ *
+ * Higher order function that return a stringValidationFunction checking that its input
+ * is a number. To be valid, a number should be finite.
+ *
+ * @param errorMessage: optional. An error to display if the input is a number.
+ *                      Use %s in the error string to display the input in the error message.
+ *
+ * @return a stringValidationFunction that verify if the input is a number.
+ */
 export const numberValidation = (errorMessage?: string): stringValidationFunction => {
     return async (input: string): Promise<true | string> => {
         if (!isFiniteNumber(input)) {
