9fbcb63badea044c995bbd0832173a2613795bf5
Leddzip <leddzip@gmail.com>
2023-02-21 21:48:50 +0100

    feature/CLOWDER-41_process-destination-path-helper
    test: finalizing scenarios for renameAll

--- src/destination-path-processor.test.ts
+++ src/destination-path-processor.test.ts
@@ -126,75 +126,86 @@ describe('renameAll', () => {
     class Given {
         public readonly pathString: string;
         public readonly replaceFromTo: FromTo[];
-        public readonly newNameForReplacement: string;
 
-        constructor(pathString: string, replaceFromTo: FromTo[], newNameForReplacement: string) {
+        constructor(pathString: string, replaceFromTo: FromTo[]) {
             this.pathString = pathString;
             this.replaceFromTo = replaceFromTo;
-            this.newNameForReplacement = newNameForReplacement;
         }
     }
     describe('Given a relative path', () => {
         describe.each([
             [
                 'given no duplicate in the path, one name in input, but not present in the path',
-                new Given('', [{ from: '', to: '' }], ''),
-                'expected'
+                new Given('this/is/my/folder/with/one/file.ts', [{ from: 'your', to: 'whatever' }]),
+                'this/is/my/folder/with/one/file.ts'
             ],
             [
                 'given no duplicate in the path, one name in input, present in the path but as substring',
-                new Given('', [{ from: '', to: '' }], ''),
-                'expected'
+                new Given('this/is/my/folder/with/one/file.your.ts', [{ from: 'your', to: 'whatever' }]),
+                'this/is/my/folder/with/one/file.your.ts'
             ],
             [
                 'given no duplicate in the path, one name in input, present in the path',
-                new Given('', [{ from: '', to: '' }], ''),
-                'expected'
+                new Given('this/is/MY/folder/with/one/file.ts', [{ from: 'MY', to: 'YOUR' }]),
+                'this/is/YOUR/folder/with/one/file.ts'
             ],
             [
                 'given some duplicate in the path, one name in input, but not present in the path',
-                new Given('', [{ from: '', to: '' }], ''),
-                'expected'
+                new Given('this/is/my/folder/and/so/is/this/with/one/file.ts', [{ from: 'two', to: 'whatever' }]),
+                'this/is/my/folder/and/so/is/this/with/one/file.ts'
             ],
             [
                 'given some duplicate in the path, one name in input, present in the path but as substring',
-                new Given('', [{ from: '', to: '' }], ''),
-                'expected'
+                new Given('this/is/my/folder/and/so/is/this/with/oneOrTwo/file.ts', [{ from: 'Two', to: 'So' }]),
+                'this/is/my/folder/and/so/is/this/with/oneOrTwo/file'
             ],
             [
                 'given some duplicate in the path, one name in input, present in the path but only one time',
-                new Given('', [{ from: '', to: '' }], ''),
-                'expected'
+                new Given('this/is/MY/folder/and/so/is/this/with/one/file.ts', [{ from: 'MY', to: 'YOUR' }]),
+                'this/is/YOUR/folder/and/so/is/this/with/one/file.ts'
             ],
             [
                 'given some duplicate in the path, one name in input, present in the path but one time as substring, one time as path element',
-                new Given('', [{ from: '', to: '' }], ''),
-                'expected'
+                new Given('this/is/MY/folder/and/so/is/this/with/one/file.MY.ts', [{ from: 'MY', to: 'YOUR' }]),
+                'this/is/YOUR/folder/and/so/is/this/with/one/file.MY.ts'
             ],
             [
                 'given some duplicate in the path, one name in input, present in the path multiple time not as substring',
-                new Given('', [{ from: '', to: '' }], ''),
-                'expected'
+                new Given('THIS/is/ny/folder/and/so/is/THIS/with/one/file.ts', [{ from: 'THIS', to: 'THAT' }]),
+                'THAT/is/my/folder/and/so/is/THAT/with/one/file.ts'
             ],
             [
                 'given some duplicate in the path, one name in input, present in the path multiple time not as substring, and as substring',
-                new Given('', [{ from: '', to: '' }], ''),
-                'expected'
+                new Given('thIS/IS/my/folder/and/so/IS/thIS/with/one/file.ts', [{ from: 'IS', to: 'SHOULD_BE' }]),
+                'thIS/SHOULD_BE/my/folder/and/so/SHOULD_BE/thIS/with/one/file.ts'
             ],
             [
                 'given some duplicate in the path, multiple name in input, none present in the path',
-                new Given('', [{ from: '', to: '' }], ''),
-                'expected'
+                new Given('this/is/my/folder/and/so/is/this/with/one/file.ts', [
+                    { from: 'java', to: 'ts' },
+                    { from: 'py', to: 'ts' },
+                    { from: 'kt', to: 'ts' }
+                ]),
+                'this/is/my/folder/and/so/is/this/with/one/file.ts'
             ],
             [
                 'given some duplicate in the path, multiple name in input, some present in the path but as substrings',
-                new Given('', [{ from: '', to: '' }], ''),
-                'expected'
+                new Given('this/is/my/folder/and/so/is/this/with/one/file.TS', [
+                    { from: 'TS', to: 'whatever' },
+                    { from: 'java', to: 'whatever' },
+                    { from: 'kt', to: 'whatever' }
+                ]),
+                'this/is/my/folder/and/so/is/this/with/one/file.TS'
             ],
             [
                 'given some duplicate in the path, multiple name in input, some present in the path as path element and as substring, others not present in the path',
-                new Given('', [{ from: '', to: '' }], ''),
-                'expected'
+                new Given('thIS/IS/MY/folder/and/so/IS/thIS/with/one/file.TS', [
+                    { from: 'IS', to: 'SHOULD_BE' },
+                    { from: 'TS', to: 'JAVA' },
+                    { from: 'MY', to: 'YOUR' },
+                    { from: 'kt', to: 'whatever' }
+                ]),
+                'thIS/SHOULD_BE/YOUR/folder/and/so/SHOULD_BE/thIS/with/one/file.TS'
             ]
         ])('Given the context "%s"', (context: string, given: Given, expected: string) => {
             describe('When I call the "renameAll" HoF on this input and use the resulting function to modify the given path', () => {
