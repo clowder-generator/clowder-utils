842b4ad9d14124d4a22b08edc620c4639481e9cc
Leddzip <leddzip@gmail.com>
2023-03-09 20:56:00 +0100

    feature/CLOWDER-45_define-interface-for-question-to-hold-template-requirements
    feat: defining Context interface and some methods to merge context
    
    test: starting to write the test layout for TDD

---
+++ src/context.test.ts
@@ -0,0 +1,12 @@
+describe('mergeTemplateContext', () => {
+    describe('Given two contexts', () => {
+        describe('with non overlapping template context', () => {});
+        describe('with overlapping template context', () => {
+            describe('and no overlapping merge strategy', () => {});
+            describe('and an overlapping merge strategy', () => {});
+        });
+        describe('with one undefined template context', () => {});
+    });
+});
+describe('mergeTemplatePath', () => {});
+describe('mergeDestinationPathProcessor', () => {});
--- src/context.ts
+++ src/context.ts
@@ -4,5 +4,17 @@ import { DestinationPathProcessor } from './destination-path-processor';
 export interface Context {
     templateContext: () => ITemplateData;
     templatePath: () => string | string[];
-    destinationPathProcessor?: () => DestinationPathProcessor;
+    destinationPathProcessor?: () => [string, string | undefined | null] | Array<[string, string | undefined | null]>;
 }
+
+export const mergeTemplateContext = (...context: Context[]): ITemplateData | undefined => {
+    return undefined;
+};
+
+export const mergeTemplatePath = (...context: Context[]): string | string[] => {
+    return '';
+};
+
+export const mergeDestinationPathProcessor = (...context: Context[]): DestinationPathProcessor => {
+    return undefined;
+};
