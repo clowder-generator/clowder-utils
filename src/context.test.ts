f39a9ea866cdd1a5cc86ec4a600f36f36229ceb3
Leddzip <leddzip@gmail.com>
2023-03-10 23:39:13 +0100

    feature/CLOWDER-45_define-interface-for-question-to-hold-template-requirements
    test: starting to write the test layout for TDD

--- src/context.test.ts
+++ src/context.test.ts
@@ -1,12 +1,18 @@
+// import { mock } from 'ts-mockito';
+// import { Context } from './context';
+
 describe('mergeTemplateContext', () => {
     describe('Given two contexts', () => {
-        describe('with non overlapping template context', () => {});
-        describe('with overlapping template context', () => {
+        // const context1: Context = mock({} as Context);
+
+        describe('with no conflicted template context', () => {});
+        describe('with conflicted template context', () => {
             describe('and no overlapping merge strategy', () => {});
             describe('and an overlapping merge strategy', () => {});
         });
         describe('with one undefined template context', () => {});
+        describe('with two undefined template context', () => {});
     });
 });
-describe('mergeTemplatePath', () => {});
-describe('mergeDestinationPathProcessor', () => {});
+xdescribe('mergeTemplatePath', () => {});
+xdescribe('mergeDestinationPathProcessor', () => {});
