3855c2b38fe95a35ff5f12bbba9f8eff6a563859
Leddzip <leddzip@gmail.com>
2023-03-14 17:34:01 +0100

    CLOWDER-45 define interface for question to hold every thing required to provide template require...
    Starting layouting for mergeTemplatePath

--- src/context.test.ts
+++ src/context.test.ts
@@ -1,8 +1,21 @@
 import { instance, mock, when } from 'ts-mockito';
-import { Context, mergeTemplateContext, silentIfSameValue, TemplateContextMergeConflictError } from './context';
+import {
+    Context,
+    mergeTemplateContext,
+    mergeTemplatePath,
+    silentIfSameValue,
+    TemplateContextMergeConflictError
+} from './context';
 import { ITemplateData } from './yeoman-helper';
 
 describe('mergeTemplateContext', () => {
+    let result: ITemplateData | undefined;
+    let exception: Error | undefined;
+    afterEach(() => {
+        result = undefined;
+        exception = undefined;
+    });
+
     describe('Given two contexts', () => {
         const context1: Context = mock<Context>();
         const context2: Context = mock<Context>();
@@ -25,8 +38,6 @@ describe('mergeTemplateContext', () => {
             });
 
             describe('When I call "mergeTemplateContext" on them', () => {
-                let result: ITemplateData | undefined;
-                let exception: Error | undefined;
                 beforeEach(() => {
                     try {
                         result = mergeTemplateContext(instance(context1), instance(context2));
@@ -34,11 +45,6 @@ describe('mergeTemplateContext', () => {
                         exception = error as Error;
                     }
                 });
-                afterEach(() => {
-                    result = undefined;
-                    exception = undefined;
-                });
-
                 test('Then I got no error', () => {
                     expect(exception).toBeUndefined();
                 });
@@ -66,18 +72,12 @@ describe('mergeTemplateContext', () => {
             });
 
             describe('and no overlapping merge strategy', () => {
-                let result: ITemplateData | undefined;
-                let exception: Error | undefined;
                 beforeEach(() => {
                     try {
                         result = mergeTemplateContext(instance(context1), instance(context2));
                     } catch (error: unknown) {
                         exception = error as Error;
                     }
-                });
-                afterEach(() => {
-                    result = undefined;
-                    exception = undefined;
                 });
                 test('Then I do not get a result', () => {
                     expect(result).toBeUndefined();
@@ -93,18 +93,12 @@ describe('mergeTemplateContext', () => {
                 });
             });
             describe('and an overlapping merge strategy "silentIfSameValue"', () => {
-                let result: ITemplateData | undefined;
-                let exception: Error | undefined;
                 beforeEach(() => {
                     try {
                         result = mergeTemplateContext(silentIfSameValue, instance(context1), instance(context2));
                     } catch (error: unknown) {
                         exception = error as Error;
                     }
-                });
-                afterEach(() => {
-                    result = undefined;
-                    exception = undefined;
                 });
                 test('Then I should not get an exception', () => {
                     expect(exception).toBeUndefined();
@@ -132,18 +126,12 @@ describe('mergeTemplateContext', () => {
             });
 
             describe('and no overlapping merge strategy', () => {
-                let result: ITemplateData | undefined;
-                let exception: Error | undefined;
                 beforeEach(() => {
                     try {
                         result = mergeTemplateContext(instance(context1), instance(context2));
                     } catch (error: unknown) {
                         exception = error as Error;
                     }
-                });
-                afterEach(() => {
-                    result = undefined;
-                    exception = undefined;
                 });
                 test('Then I do not get a result', () => {
                     expect(result).toBeUndefined();
@@ -159,18 +147,12 @@ describe('mergeTemplateContext', () => {
                 });
             });
             describe('and an overlapping merge strategy "silentIfSameValue"', () => {
-                let result: ITemplateData | undefined;
-                let exception: Error | undefined;
                 beforeEach(() => {
                     try {
                         result = mergeTemplateContext(silentIfSameValue, instance(context1), instance(context2));
                     } catch (error: unknown) {
                         exception = error as Error;
                     }
-                });
-                afterEach(() => {
-                    result = undefined;
-                    exception = undefined;
                 });
                 test('Then I do not get a result', () => {
                     expect(result).toBeUndefined();
@@ -188,5 +170,91 @@ describe('mergeTemplateContext', () => {
         });
     });
 });
-xdescribe('mergeTemplatePath', () => {});
+xdescribe('mergeTemplatePath', () => {
+    let result: string | string[] | undefined;
+    let exception: Error | undefined;
+    afterEach(() => {
+        result = undefined;
+        exception = undefined;
+    });
+    describe('Given one context', () => {
+        let context: Context;
+        beforeEach(() => {
+            context = mock<Context>();
+        });
+        describe('and the context has a single templatePath', () => {
+            beforeEach(() => {
+                when(context.templatePath).thenReturn(() => 'element1');
+            });
+            describe('When I call "mergeTemplatePath"', () => {
+                beforeEach(() => {
+                    try {
+                        result = mergeTemplatePath(instance(context));
+                    } catch (error: unknown) {
+                        exception = error as Error;
+                    }
+                });
+                test('Then there is no error', () => {
+                    expect(exception).toBeUndefined();
+                });
+                test('Then the resulting templatePath is defined', () => {
+                    expect(result).not.toBeUndefined();
+                });
+                test('Then the resulting templatePath is as expected', () => {
+                    expect(result).toEqual('element1');
+                });
+            });
+        });
+        describe('and the context has an array of templatePath', () => {
+            describe('and this array of templatePath contains unique elements', () => {
+                beforeEach(() => {
+                    when(context.templatePath).thenReturn(() => ['element1', 'element2']);
+                });
+                describe('When I call "mergeTemplatePath"', () => {
+                    beforeEach(() => {
+                        try {
+                            result = mergeTemplatePath(instance(context));
+                        } catch (error: unknown) {
+                            exception = error as Error;
+                        }
+                    });
+                    test('Then there is no error', () => {
+                        expect(exception).toBeUndefined();
+                    });
+                    test('Then the resulting templatePath is defined', () => {
+                        expect(result).not.toBeUndefined();
+                    });
+                    test('Then the resulting templatePath is an array', () => {
+                        expect(result).toBeInstanceOf(Array);
+                    });
+                    test('Then the resulting templatePath contains 2 elements', () => {
+                        expect((result as string[]).length).toEqual(2);
+                    });
+                    test('Then the resulting templatePath is as expected', () => {
+                        expect(result).toEqual(['element1', 'element2']);
+                    });
+                });
+            });
+            describe('and this array of templatePath contains not only unique elements', () => {});
+        });
+    });
+    describe('Given two context', () => {
+        describe('and the first context has a single templatePath', () => {
+            describe('and the second context contains a single templatePath', () => {
+                describe('and there is no overlap between templatePath of each context', () => {});
+                describe('and there is overlap between templatePath of each context', () => {});
+            });
+            describe('and the second context contains an array of templatePath', () => {
+                describe('and there is no overlap between templatePath of each context', () => {});
+                describe('and there is overlap between templatePath of each context', () => {});
+            });
+        });
+        describe('and the first context has an array of templatePath', () => {});
+    });
+    describe('Given 3 context', () => {
+        describe('and each containing only a single templatePath', () => {});
+        describe('and each containing an array of templatePath', () => {});
+        describe('and all of them contains empty array', () => {});
+    });
+});
 xdescribe('mergeDestinationPathProcessor', () => {});
